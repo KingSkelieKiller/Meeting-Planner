f093a0b697d3878bb97735de9b3f7b50dd1af451
KingSkeletonKiller <kenneth.acsp@gmail.com>
2024-07-06 11:33:31 +0800

    Comments on how it works and description of what it does

--- A Python level 5 application.py
+++ A Python level 5 application.py
@@ -6,6 +6,7 @@ root = Tk()
 
 root.geometry("600x600")
 
+#Tells the program the date
 yy = int(input("What year is it?"))
 mm = int(input("What month is it in number form?"))
 dd = int(input("What is the date?"))
@@ -14,11 +15,14 @@ cal = Calendar(root, selectmode = 'day', year = yy, month = mm, day = dd)
 
 cal.pack(pady = 10)
 
+#Creates a variable to store the event
 var = StringVar()
 
+#Creates a text file to store family events
 def grad_date():
     booking = open('test.txt', 'a')
     event = var.get()
+    #Saves event in file
     booking.write(f"{event}, {cal.get_date()}\n")
     booking.close()
     with open(r'test.txt', 'r') as booking:
@@ -32,6 +36,7 @@ def grad_date():
     cal.tag_config('event', background="red", foreground="white")
     var.set("")
 
+#Creates button to run function for saving non-family events and creates entry for input
 Button(root, text = "Save entry", command = grad_date).pack(pady = 20)
 label_entry = Label(root, text="Enter event").pack(pady=10)
 event_entry = Entry(root, textvariable=var).pack(pady=10)
@@ -39,7 +44,7 @@ event_entry = Entry(root, textvariable=var).pack(pady=10)
 date = Label(root, text = "")
 date.pack(pady = 10)
 
-
+#Checking file for previously saved non-family events
 with open(r'family.txt', 'r') as booking:
     lines = booking.readlines()
     for line in lines:
@@ -50,12 +55,14 @@ with open(r'family.txt', 'r') as booking:
         cal.tag_config('event', background="red", foreground="white")
 
 
-
+#Creates variable for family events
 var2 = StringVar()
 
+#Creates a text file to store family events
 def grad_date_2():
     booking2 = open('family.txt', 'a')
     event2 = var2.get()
+    #Saves event in file
     booking2.write(f"{event2}, {cal.get_date()}\n")
     booking2.close()
     with open(r'family.txt', 'r') as booking2:
@@ -69,6 +76,7 @@ def grad_date_2():
     cal.tag_config('event2', background="green", foreground="white")
     var2.set("")
 
+#Creates button to run function for saving family events and creates entry for input
 Button(root, text = "Save entry (family)", command = grad_date_2).pack(pady = 20)
 family_label_entry = Label(root, text="Enter family event").pack(pady=10)
 family_event_entry = Entry(root, textvariable=var2).pack(pady=10)
@@ -76,7 +84,7 @@ family_event_entry = Entry(root, textvariable=var2).pack(pady=10)
 date = Label(root, text = "")
 date.pack(pady = 10)
 
-
+#Checking file for previously saved family events
 with open(r'family.txt', 'r') as booking:
     lines = booking.readlines()
     for line in lines:
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Meeting Planner
+## Function
+The program is designed to help families organise events together. It helps tell them when someone has an individual event and when there is a family event.
+## How it works
+1) The program asks for the date, year and month to know what day it is
+2) It generates the calendar and two entry boxes and two buttons
+3) One set of entry box and button is for inputting a family event
+4) Another is for family events
+5) When a family event is input, the box with the date where it was input turns green
+6) For non-family events, the box turns red
+7) This allows people to know when there are non-family events to help plan around those events
